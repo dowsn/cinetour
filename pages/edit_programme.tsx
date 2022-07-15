482ddbb923b8e933e00ccf190c4a546043d18f3f
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-07-15 21:37:06 +0200

    Added styles for error

--- pages/edit_programme.tsx
+++ pages/edit_programme.tsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
 import { GetServerSidePropsContext } from 'next';
 import Link from 'next/link';
 import { Fragment, useState } from 'react';
