e9f55d5afbcff6201bc089abf179ab62dfa10a6d
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-09-02 11:22:17 +0200

    .env file updated

---
+++ components/UploadImage.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { generateSignature } from '../utils/cloudinarySignature';
+
+export function ImageUpload() {
+  const [isImageUploaded, setIsImageUploaded] = useState(false);
+  async function handleWidgetClick() {
+    const widget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: 'kennyy',
+        uploadSignature: generateSignature,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        resourceType: 'image',
+      },
+      (error, result) => {
+        if (!error && result && result.event === 'success') {
+          console.log('Done! Here is the image info: ', result.info);
+          setIsImageUploaded(true);
+        } else if (error) {
+          console.log(error);
+        }
+      },
+    );
+    widget.open();
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.vertical}>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handleWidgetClick}
+        >
+          Upload image
+        </button>
+      </div>
+
+      {isImageUploaded ? <div>Successfully uploaded</div> : null}
+    </div>
+  );
+}
---
+++ utils/cloudinarySignature.js
@@ -0,0 +1,13 @@
+export function generateSignature(callback, paramsToSign) {
+  fetch(`/api/cloudinary`, {
+    method: 'POST',
+    body: JSON.stringify({
+      paramsToSign,
+    }),
+  })
+    .then((r) => r.json())
+    .then(({ signature }) => {
+      callback(signature);
+    });
+    .catch()
+}
